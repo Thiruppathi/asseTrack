5e6108a6d7fe737854adfd95fad8296dd980d37f
Thiru <thiru.androider@gmail.com>
2016-01-26 00:13:59 +0000

    user-card to fetch db details from env.js

--- app/scripts/env.js
+++ app/scripts/env.js
@@ -1,7 +1,7 @@
 var env = {
   db: {
     baseUrl: 'https://assetrack.firebaseio.com',
-    users: 'https://assetrack.firebaseio.com/users',
-    assets: 'https://assetrack.firebaseio.com/assets'
+    users: 'https://assetrack.firebaseio.com/users/',
+    assets: 'https://assetrack.firebaseio.com/assets/'
   }
 };

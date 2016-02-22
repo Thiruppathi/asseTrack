76a1d791bb27403fbc4bd37147d4a2ff8336c3e6
Thiru <thiru.androider@gmail.com>
2016-02-22 17:11:19 +0000

    asset creation changes. landing page changes. photos for devices. env changes. robots.txt to block search engine result.

--- app/scripts/env.js
+++ app/scripts/env.js
@@ -1,10 +1,21 @@
 var env = {
   db: {
+    baseUrl: 'https://myjarvis.firebaseio.com',
+    users: 'https://myjarvis.firebaseio.com/users/',
+    assets: 'https://myjarvis.firebaseio.com/assets/',
+    history: 'https://myjarvis.firebaseio.com/history/'
+  }
+};
+
+/* Dev Environment */
+/*
+var env = {
+  db: {
     baseUrl: 'https://assetrack.firebaseio.com',
     users: 'https://assetrack.firebaseio.com/users/',
     assets: 'https://assetrack.firebaseio.com/assets/',
     history: 'https://assetrack.firebaseio.com/history/'
   }
-};
+};*/
 
 console.log(env);

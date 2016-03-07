015472db718e847b0a7579b75636fb39585f86ed
Thiru <thiru.androider@gmail.com>
2016-03-07 09:57:23 +0000

    Enabling Service worker

--- gulpfile.js
+++ gulpfile.js
@@ -310,7 +310,7 @@ gulp.task('default', ['clean'], function(cb) {
   runSequence(
     ['copy', 'styles'],
     'elements', ['lint', 'images', 'fonts', 'html'],
-    'vulcanize', // 'cache-config',
+    'vulcanize', 'cache-config',
     cb);
 });
 
